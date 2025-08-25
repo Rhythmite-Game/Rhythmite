39e21c95fa177d0517146888854f50c1b0e8706d
DoubleDotStudios <doubledotgames@gmail.com>
2025-08-26 00:28:08 +1200

    added key detection and player initialisation

--- src/main.go
+++ src/main.go
@@ -4,8 +4,10 @@ import (
 	"image/color"
 	"log"
 
+	"github.com/Rhythmite-Game/Rhythmite/classes"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
@@ -15,18 +17,41 @@ const (
 )
 
 var (
-	playerPlaceholder *ebiten.Image
+	player *classes.Entity
 )
 
-type Game struct{}
+func init() {
+	player = &classes.Entity{
+		// Data
+		Name:      "Player",
+		PositionX: screenWidth / 2,
+		PositionY: screenHeight / 2,
+
+		// Stats
+		Health:            10,
+		BaseAttack:        1,
+		AttackMultiplier:  1,
+		BaseDefense:       0,
+		DefenseMultiplier: 1,
+
+		// Movement
+		TileSplits:     0,
+		LegalDiagonals: false,
+	}
+}
+
+type Game struct {
+	keys []ebiten.Key
+}
 
 func (g *Game) Update() error {
+	g.keys = inpututil.AppendPressedKeys(g.keys[:0])
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	ebitenutil.DebugPrint(screen, "Hello, World!")
-	vector.DrawFilledCircle(screen, 100, 100, 20, color.RGBA{255, 50, 50, 100}, false)
+	vector.DrawFilledCircle(screen, player.PositionX, player.PositionY, 20, color.RGBA{255, 50, 50, 100}, false)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
