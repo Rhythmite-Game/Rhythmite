b80c6e251000897e8cfeb3f400bbaa7eb65203fe
DoubleDotStudios <doubledotgames@gmail.com>
2025-08-26 00:27:41 +1200

    fixed property capitalisation for use externaly

--- src/classes/Enitity.go
+++ src/classes/Enitity.go
@@ -2,18 +2,18 @@ package classes
 
 type Entity struct {
 	// Data
-	name      string  // Name of entity, eg. Player
-	positionX float64 // Global x position of the entity
-	positionY float64 // Global y position of the entity
+	Name      string  // Name of entity, eg. Player
+	PositionX float32 // Global x position of the entity
+	PositionY float32 // Global y position of the entity
 
 	// Stats
-	health            float64 // Health of the entity, eg. 10HP, value of -404 grants invincibility
-	baseAttack        float64 // Base damage dealt by an entity
-	attackMultiplier  float64 // Number to multiply damage by, eg. <1 weakens attacks and >1 strengthens attacks
-	baseDefence       float64 // Base damage reduction, eg. 0.5 means attacks of 2 damage deal 1.5 damage instead
-	defenceMultiplier float64 // Number to multiply defence by, eg. <1 decreases defence and >1 increases defence
+	Health            float32 // Health of the entity, eg. 10HP, value of -404 grants invincibility
+	BaseAttack        float32 // Base damage dealt by an entity
+	AttackMultiplier  float32 // Number to multiply damage by, eg. <1 weakens attacks and >1 strengthens attacks
+	BaseDefense       float32 // Base damage reduction, eg. 0.5 means attacks of 2 damage deal 1.5 damage instead
+	DefenseMultiplier float32 // Number to multiply defense by, eg. <1 decreases defence and >1 increases defence
 
 	// Movement
-	tileSplits     int  // Number of splits an entity sees on a tile, eg. 1 split = 4 subtiles per tile
-	legalDiagonals bool // Whether or not diagonal movement is legalised for the entity
+	TileSplits     int  // Number of splits an entity sees on a tile, eg. 1 split = 4 subtiles per tile
+	LegalDiagonals bool // Whether or not diagonal movement is legalised for the entity
 }
