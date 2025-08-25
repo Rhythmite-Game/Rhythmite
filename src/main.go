4a83769bd4fc85905522aaf14b68859976bf206f
DoubleDotStudios <doubledotgames@gmail.com>
2025-08-26 00:14:02 +1200

    setup ebitengine with a small graphics demo

--- src/main.go
+++ src/main.go
@@ -1,1 +1,42 @@
 package main
+
+import (
+	"image/color"
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/vector"
+)
+
+const (
+	screenWidth  = 640
+	screenHeight = 370
+)
+
+var (
+	playerPlaceholder *ebiten.Image
+)
+
+type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Hello, World!")
+	vector.DrawFilledCircle(screen, 100, 100, 20, color.RGBA{255, 50, 50, 100}, false)
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
+}
+
+func main() {
+	ebiten.SetWindowSize(screenWidth*2, screenHeight*2)
+	ebiten.SetWindowTitle("Rhythmite")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
+}
