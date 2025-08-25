a213f9a1f1f7b94a8c2a0d4c757a1522d4422d33
DoubleDotStudios <doubledotgames@gmail.com>
2025-08-26 01:15:51 +1200

    implemented basic player movement

--- src/main.go
+++ src/main.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/Rhythmite-Game/Rhythmite/classes"
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
 	"github.com/hajimehoshi/ebiten/v2/vector"
 )
@@ -14,6 +13,7 @@ import (
 const (
 	screenWidth  = 640
 	screenHeight = 370
+	tileSize     = 32
 )
 
 var (
@@ -33,10 +33,12 @@ func init() {
 		AttackMultiplier:  1,
 		BaseDefense:       0,
 		DefenseMultiplier: 1,
+		SpeedMultiplier:   1,
 
 		// Movement
 		TileSplits:     0,
 		LegalDiagonals: false,
+		DashEnabled:    false,
 	}
 }
 
@@ -45,13 +47,21 @@ type Game struct {
 }
 
 func (g *Game) Update() error {
+	player.Move(tileSize)
 	g.keys = inpututil.AppendPressedKeys(g.keys[:0])
+
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
-	vector.DrawFilledCircle(screen, player.PositionX, player.PositionY, 20, color.RGBA{255, 50, 50, 100}, false)
+	var keyNames []string
+	for _, k := range g.keys {
+		keyNames = append(keyNames, k.String())
+	}
+
+	player.Dash(keyNames)
+
+	vector.DrawFilledCircle(screen, player.PositionX, player.PositionY, 20, color.RGBA{10, 100, 200, 100}, false)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
